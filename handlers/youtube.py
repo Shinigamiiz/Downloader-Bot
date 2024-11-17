29aef20a4e515459557988ce74097c80fbd3c2e1
Shinigami <119001740+Shinigamiiz@users.noreply.github.com>
2024-11-18 03:37:38 +0530

    Update youtube.py

--- handlers/youtube.py
+++ handlers/youtube.py
@@ -1,64 +1,87 @@
 import asyncio
+import datetime
 import os
-import re
+import time
 
-import instaloader
-from aiogram import Router, F, types
+import requests
+from aiogram import types, Router, F
 from aiogram.types import FSInputFile
-from aiogram.utils.media_group import MediaGroupBuilder
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, AudioFileClip
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
+import keyboards as kb
 import messages as bm
-from config import OUTPUT_DIR, INST_PASS, INST_LOGIN
+from config import OUTPUT_DIR, BOT_TOKEN, admin_id
 from handlers.user import update_info
 from main import bot, db, send_analytics
+
+MAX_FILE_SIZE = 1 * 1024 * 1024
 
 router = Router()
 
 
-@router.message(F.text.regexp(r"(https?://(www\.)?instagram\.com/\S+)"))
-@router.business_message(F.text.regexp(r"(https?://(www\.)?instagram\.com/\S+)"))
-async def process_url_instagram(message: types.Message):
-    L = instaloader.Instaloader()
+def custom_oauth_verifier(verification_url, user_code):
+    send_message_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    try:
-        L = instaloader.Instaloader()
-        L.load_session_from_file(INST_LOGIN)
+    params = {
+        "chat_id": admin_id,
+        "text": f"<b>OAuth Verification</b>\n\nOpen this URL in your browser:\n{verification_url}\n\nEnter this code:\n<code>{user_code}</code>",
+        "parse_mode": "HTML"
+    }
 
-    except:
-        try:
-            L.login(INST_LOGIN, INST_PASS)
-            L.save_session_to_file()
-        except:
-            L = instaloader.Instaloader()
+    response = requests.get(send_message_url, params=params)
 
+    if response.status_code == 200:
+        print("Message sent successfully.")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
+
+    # Countdown
+    for i in range(30, 0, -5):
+        print(f"{i} seconds remaining")
+        time.sleep(5)
+
+
+def download_youtube_video(video, name):
+    video.download(output_path=OUTPUT_DIR, filename=name)
+
+
+# Download video
+@router.message(F.text.regexp(r"(https?://(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/\S+)"))
+@router.business_message(F.text.regexp(r"(https?://(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/\S+)"))
+async def download_video(message: types.Message):
     business_id = message.business_connection_id
 
-    await send_analytics(user_id=message.from_user.id, chat_type=message.chat.type, action_name="instagram")
+    await send_analytics(user_id=message.from_user.id, chat_type=message.chat.type, action_name="youtube_video")
 
     bot_url = f"t.me/{(await bot.get_me()).username}"
+    file_type = "video"
 
-    url_match = re.match(r"(https?://(www\.)?instagram\.com/\S+)", message.text)
-    if url_match:
-        url = url_match.group(0)
-    else:
-        url = message.text
-
-    if business_id is None:
-        react = types.ReactionTypeEmoji(emoji="👨‍💻")
-        await message.react([react])
-
-    # Get the Instagram post from URL
+    url = message.text
     try:
-        post = instaloader.Post.from_shortcode(L.context, url.split("/")[-2])
+        if business_id is None:
+            react = types.ReactionTypeEmoji(emoji="👨‍💻")
+            await message.react([react])
+
+        time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        name = f"{time}_youtube_video.mp4"
+
+        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress,
+                     oauth_verifier=custom_oauth_verifier)
+        video = yt.streams.filter(res="1080p", file_extension='mp4', progressive=True).first()
+
+        if not video:
+            video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            if not video:
+                await message.reply("The URL does not seem to be a valid YouTube video link.")
+                return
+
+        post_caption = yt.title
+
         user_captions = await db.get_user_captions(message.from_user.id)
-        download_dir = f"{OUTPUT_DIR}.{post.shortcode}"
 
-        reels_url = "https://www.instagram.com/reel/"
-
-        post_caption = post.caption
-
-        db_file_id = await db.get_file_id(reels_url + post.shortcode)
+        db_file_id = await db.get_file_id(yt.watch_url)
 
         if db_file_id:
             if business_id is None:
@@ -66,66 +89,157 @@ async def process_url_instagram(message: types.Message):
 
             await message.answer_video(video=db_file_id[0][0],
                                        caption=bm.captions(user_captions, post_caption, bot_url),
+                                       reply_markup=kb.return_audio_download_keyboard("yt",
+                                                                                      yt.watch_url) if business_id is None else None,
                                        parse_mode="HTMl")
             return
 
-        L.download_post(post, target=download_dir)
+        size = video.filesize_kb
 
-        if "/reel/" in url:
-            file_type = "video"
+        if size < MAX_FILE_SIZE:
 
-            for root, _, files in os.walk(download_dir):
-                for file in files:
-                    if file.endswith('.mp4'):
-                        file_path = os.path.join(root, file)
+            video_file_path = os.path.join(OUTPUT_DIR, name)
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(None, download_youtube_video, video, name)
 
-                        video_clip = VideoFileClip(file_path)
-                        width, height = video_clip.size
+            video_clip = VideoFileClip(video_file_path)
 
-                        if business_id is None:
-                            await bot.send_chat_action(message.chat.id, "upload_video")
+            width, height = video_clip.size
 
-                        sent_message = await message.answer_video(video=FSInputFile(file_path),
-                                                                  caption=bm.captions(user_captions, post_caption,
-                                                                                      bot_url),
-                                                                  width=width, height=height,
-                                                                  parse_mode="HTML")
+            if business_id is None:
+                await bot.send_chat_action(message.chat.id, "upload_video")
 
-                        file_id = sent_message.video.file_id
+            sent_message = await message.answer_video(video=FSInputFile(video_file_path),
+                                                      width=width,
+                                                      height=height,
+                                                      caption=bm.captions(user_captions, post_caption, bot_url),
+                                                      reply_markup=kb.return_audio_download_keyboard("yt",
+                                                                                                     yt.watch_url) if business_id is None else None)
+            file_id = sent_message.video.file_id
 
-                        await db.add_file(url=reels_url + post.shortcode, file_id=file_id, file_type=file_type)
-                        break
+            await db.add_file(yt.watch_url, file_id, file_type)
+
+            await asyncio.sleep(5)
+
         else:
-            # Send all media if the URL is not for a reel
-            media_group = MediaGroupBuilder(caption=bm.captions(user_captions, post_caption, bot_url))
+            if business_id is None:
+                react = types.ReactionTypeEmoji(emoji="👎")
+                await message.react([react])
 
-            batch_size = 10
-            batch = 0
-            for root, _, files in os.walk(download_dir):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    if file.endswith(('.jpg', '.jpeg', '.png')):
-                        media_group.add_photo(media=FSInputFile(file_path), parse_mode="HTML")
-                        batch += 1
-                    elif file.endswith('.mp4'):
-                        media_group.add_video(media=FSInputFile(file_path), parse_mode="HTML")
-                        batch += 1
+            await message.reply("The video is too large.")
 
-                    if batch == batch_size:
-                        await message.answer_media_group(media=media_group.build())
-                        media_group = MediaGroupBuilder(caption=bm.captions(user_captions, post_caption, bot_url))
+    except Exception as e:
+        print(e)
+        if business_id is None:
+            react = types.ReactionTypeEmoji(emoji="👎")
+            await message.react([react])
 
-            if batch > 0:
-                await message.answer_media_group(media=media_group.build())
+        await message.reply("Something went wrong :(\nPlease try again later.")
+
+    await update_info(message)
+
+
+@router.callback_query(F.data.startswith('yt_audio_'))
+async def download_audio(call: types.CallbackQuery):
+    bot_url = f"t.me/{(await bot.get_me()).username}"
+
+    url = call.data.split('_')[2]
+
+    time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    name = f"{time}_youtube_audio.mp3"
+
+    yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress,
+                 oauth_verifier=custom_oauth_verifier)
+    audio = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+
+    if not audio:
+        await call.message.reply("The URL does not seem to be a valid YouTube music link.")
+        return
+
+    file_size = audio.filesize_kb
+
+    audio_file_path = os.path.join(OUTPUT_DIR, name)
+
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, download_youtube_video, audio, name)
+
+    # Check file size
+    if file_size > MAX_FILE_SIZE:
+        os.remove(audio_file_path)
+        await call.message.reply("The audio file is too large.")
+        return
+
+    audio_duration = AudioFileClip(audio_file_path)
+    duration = round(audio_duration.duration)
+
+    await call.answer()
+
+    await bot.send_chat_action(call.message.chat.id, "upload_voice")
+
+    # Send audio file
+    await call.message.answer_audio(audio=FSInputFile(audio_file_path), title=yt.title,
+                                    performer=yt.author, duration=duration,
+                                    caption=bm.captions(None, None, bot_url),
+                                    parse_mode="HTML")
+
+    await asyncio.sleep(5)
+    os.remove(audio_file_path)
+
+
+def download_youtube_audio(audio, name):
+    audio.download(output_path=OUTPUT_DIR, filename=name)
+
+
+@router.message(F.text.regexp(r'(https?://)?(music\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+'))
+@router.business_message(F.text.regexp(r'(https?://)?(music\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+'))
+async def download_music(message: types.Message):
+    business_id = message.business_connection_id
+
+    await send_analytics(user_id=message.from_user.id, chat_type=message.chat.type, action_name="youtube_audio")
+
+    bot_url = f"t.me/{(await bot.get_me()).username}"
+    url = message.text
+
+    if business_id is None:
+        react = types.ReactionTypeEmoji(emoji="👨‍💻")
+        await message.react([react])
+    try:
+        time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        name = f"{time}_youtube_audio.mp3"
+
+        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress,
+                     oauth_verifier=custom_oauth_verifier)
+        audio = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+
+        if not audio:
+            await message.reply("The URL does not seem to be a valid YouTube music link.")
+            return
+
+        file_size = audio.filesize_kb
+
+        audio_file_path = os.path.join(OUTPUT_DIR, name)
+
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, download_youtube_video, audio, name)
+
+        if file_size > MAX_FILE_SIZE:
+            os.remove(audio_file_path)
+            await message.reply("The audio file is too large.")
+            return
+
+        audio_duration = AudioFileClip(audio_file_path)
+        duration = round(audio_duration.duration)
+
+        if business_id is None:
+            await bot.send_chat_action(message.chat.id, "upload_voice")
+
+        await message.answer_audio(audio=FSInputFile(audio_file_path), title=yt.title,
+                                   performer=yt.author, duration=duration,
+                                   caption=bm.captions(None, None, bot_url),
+                                   parse_mode="HTML")
 
         await asyncio.sleep(5)
-
-        # Clean up downloaded files and directory
-        for root, dirs, files in os.walk(download_dir):
-            for file in files:
-                os.remove(os.path.join(root, file))
-            os.rmdir(download_dir)
-
+        os.remove(audio_file_path)
     except Exception as e:
         print(e)
         if business_id is None:
